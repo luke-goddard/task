7225baae5c2e71421b2f2680e6277785811ac1bd
luke-goddard <l-goddard@protonmail.com>
2025-04-18 16:43:14 +0100

    feature add the dummy login page

--- frontend/src/routes/app.tsx
+++ frontend/src/routes/app.tsx
@@ -1,6 +1,12 @@
 import { AppSidebar } from '@/components/app-sidebar'
-import { SidebarProvider } from '@/components/ui/sidebar'
+import { ChartAreaInteractive } from '@/components/chart-area'
+import { DataTable } from '@/components/data-table'
+import { SectionCards } from '@/components/section-cards'
+import { SiteHeader } from '@/components/site-header'
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
 import { createFileRoute } from '@tanstack/react-router'
+import data from '@/components/data.json'
+
 
 export const Route = createFileRoute('/app')({
 	component: RouteComponent,
@@ -11,6 +17,20 @@ function RouteComponent() {
 		<>
 			<SidebarProvider>
 				<AppSidebar />
+				<SidebarInset>
+					<SiteHeader />
+					<div className="flex flex-1 flex-col">
+						<div className="@container/main flex flex-1 flex-col gap-2">
+							<div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+								<SectionCards />
+								<div className="px-4 lg:px-6">
+									<ChartAreaInteractive />
+								</div>
+								<DataTable data={data} />
+							</div>
+						</div>
+					</div>
+				</SidebarInset>
 			</SidebarProvider>
 		</>
 	)
